82f9129a539362480bce64a183ff4a7c2314652b
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2022-11-01 13:30:08 +0600

    Sort of works.

--- task2.go
+++ task2.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,12 +16,13 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/hello", HelloHandler)
 	mux.HandleFunc("/v1/time", CurrentTimeHandler)
+	mux.HandleFunc("/v1/encoding", AddEncoderToContext(EncodingRequestHandler))
 	//wrap entire mux with logger middleware
 	//wrappedMux := NewLogger(mux)
 
 	log.Printf("server is listening at %s", addr)
 	//use wrappedMux instead of mux as root handler
-	log.Fatal(http.ListenAndServe(addr, Logogger(mux)))
+	log.Fatal(http.ListenAndServe(addr, LoggerMiddleware(mux)))
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,9 +34,54 @@ func CurrentTimeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("the current time is %v", curTime)))
 }
 
-func Logogger(next http.Handler) http.HandlerFunc {
+func EncodingRequestHandler(w http.ResponseWriter, r *http.Request) {
+
+	var employee = EmployeeInfo{123456, "Abbaddon", "Lupercal"}
+
+	var encoder = r.Context().Value(requestDefinedEncoderKey)
+
+	xmlEnc, ok := encoder.(*xml.Encoder)
+	if ok {
+		log.Printf("Attempt to encode in xml")
+		xmlEnc.Encode(employee)
+	}
+
+	jsonEnc, ok := encoder.(*json.Encoder)
+	if ok {
+		log.Printf("Attempt to encode in json")
+		jsonEnc.Encode(employee)
+	}
+
+}
+
+func LoggerMiddleware(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
 		log.Printf("Logger middleware says: %s %s %v", r.Method, r.URL.Path, time.Now().Format(time.StampMilli))
 	}
+}
+
+type encoderKey string
+
+const requestDefinedEncoderKey encoderKey = "encoder"
+
+func AddEncoderToContext(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.Header.Get("content-type") {
+		case "xml":
+			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), requestDefinedEncoderKey, xml.NewEncoder(w))))
+		case "json":
+			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), requestDefinedEncoderKey, json.NewEncoder(w))))
+		default:
+			w.WriteHeader(400)
+		}
+	}
+
+}
+
+type EmployeeInfo struct {
+	EmployeeId int
+	FirstName  string
+	LastName   string
 }
